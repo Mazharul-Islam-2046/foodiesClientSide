b24892561876934764c1987f0345b1cb732a02ae
Mazharul-Islam-2046 <mdmazharulislam2046@gmail.com>
2025-04-14 20:11:58 +0600

    Some codes on ItemSearchResult

--- src/Pages/Menu/Sections/ItemsSearchResult/ItemSearchResult.jsx
+++ src/Pages/Menu/Sections/ItemsSearchResult/ItemSearchResult.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import FoodCard from '../../../../SharedComponents/Card/FoodCard';
 
 const ItemSearchResult = ({filteredMenuItems, isLoading, isError, error, hasNextPage, isFetchNextPage, setSearch}) => {
 
     console.log(filteredMenuItems, isLoading, isError, error, hasNextPage, isFetchNextPage, setSearch);
     return (
-        <div className='basis-2/3'>
-            
+        <div className='basis-2/3 grid grid-cols-4 gap-4'>
+            {
+                filteredMenuItems?.map((item) => <FoodCard item={item} key={item._id}/>)
+            }
         </div>
     );
 };
