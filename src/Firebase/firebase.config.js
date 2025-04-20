3a4b83996fd81c9dfd8320077c1c0dcfa7f0ee79
Mazharul-Islam-2046 <mdmazharulislam2046@gmail.com>
2025-04-20 12:59:04 +0600

    Added the Auth system

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -6,13 +6,13 @@ import { initializeApp } from "firebase/app";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCXGlaKEQyGWcbZBNqlCXILNY1r2M-FEPM",
-  authDomain: "foodies-19192.firebaseapp.com",
-  projectId: "foodies-19192",
-  storageBucket: "foodies-19192.firebasestorage.app",
-  messagingSenderId: "1047716719762",
-  appId: "1:1047716719762:web:ba2c2d05c2ce54a0920a4b",
-  measurementId: "G-M4TPR08CKK"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
